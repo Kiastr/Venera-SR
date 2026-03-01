595da0468534f5d8fad9d1eda9b0c267affff4b4
Kiastr <kiastr@users.noreply.github.com>
2026-03-01 03:27:04 -0500

    feat: remove Telegram button from About page

--- lib/pages/settings/about.dart
+++ lib/pages/settings/about.dart
@@ -49,13 +49,7 @@ class _AboutSettingsState extends State<AboutSettings> {
             launchUrlString("https://github.com/Kiastr/Venera-SSR");
           },
         ).toSliver(),
-        ListTile(
-          title: const Text("Telegram"),
-          trailing: const Icon(Icons.open_in_new),
-          onTap: () {
-            launchUrlString("https://t.me/venera_release");
-          },
-        ).toSliver(),
+
       ],
     );
   }
